d901b3272205814c1dd225075bc18f5f71ffc589
Nathan Hull <hulln@mail.gvsu.edu>
2018-04-26 15:53:24 -0400

    Got models for Company and Entry, has_many relationship

---
+++ Backend/db/migrate/20180426194734_create_entries.rb
@@ -0,0 +1,13 @@
+class CreateEntries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :entries do |t|
+      t.string :name
+      t.text :description
+      t.datetime :created_at
+      t.integer :amount
+      t.references :company, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
